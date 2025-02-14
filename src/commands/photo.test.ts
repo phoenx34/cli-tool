97db6e8ad5f2b2442873d158b72bb6d64d31dff6
phoenx34 <misaac34@gmail.com>
2025-02-14 12:41:33 -0500

    add getByPhotoName

--- src/commands/photo.test.ts
+++ src/commands/photo.test.ts
@@ -1,9 +1,10 @@
 import { Command } from "@oclif/core";
-import { getPhotoById } from "../utils/api";
+import { getAllAlbums, getPhotoById } from "../utils/api";
 import Photo from "./photo";
 
 jest.mock("../utils/api", () => ({
   getPhotoById: jest.fn(),
+  getAllAlbums: jest.fn(),
 }));
 
 describe("Photo Command", () => {
@@ -26,6 +27,30 @@ describe("Photo Command", () => {
     await Photo.run(["--id", "1"]);
 
     expect(getPhotoById).toHaveBeenCalledWith("1");
+    expect(logSpy).toHaveBeenCalledTimes(1);
+    expect(logSpy).toHaveBeenCalledWith(
+      expect.stringContaining("Photo 1")
+    );
+  });
+
+  test("should display an error message if no ID is provided", async () => {
+    await expect(Photo.run([])).rejects.toThrow("Please provide a photo ID or name");
+  });
+
+  test("should display a photo by Name", async () => {
+    (getAllAlbums as jest.Mock).mockResolvedValue([
+          {
+            albumId: 1,
+            photos: [
+              { photoId: 1, title: "Photo 1" },
+              { photoId: 2, title: "Photo 2" },
+            ],
+          },
+          { albumId: 2, photos: [{ photoId: 3, title: "Photo 3" }] },
+        ]);
+
+    await Photo.run(["--name", "Photo 1"]);
+
     expect(logSpy).toHaveBeenCalledTimes(1);
     expect(logSpy).toHaveBeenCalledWith(
       expect.stringContaining("Photo 1")
--- src/commands/photo.ts
+++ src/commands/photo.ts
@@ -1,8 +1,8 @@
 import { Flags, Command } from "@oclif/core";
-import { getPhotoById } from "../utils/api";
+import { getAllAlbums, getPhotoById } from "../utils/api";
 import TtyTable from "tty-table";
 import { TABLE_HEADERS } from "../globals";
-import { Photo } from "../types/types";
+import { Album, Photo } from "../types/types";
 
 export default class GetPhoto extends Command {
   static description: string = 'Get a photo by its ID';
@@ -12,20 +12,34 @@ export default class GetPhoto extends Command {
 
   static flags = {
     id: Flags.string({
-      char: 'i',
       description: 'ID of the photo',
-      required: true,
+      required: false,
     }),
+    name: Flags.string({
+      description: 'Name of photo or keyword in name of photo',
+      required: false,
+    })
   };
 
   async run() {
     const { flags } = await this.parse(GetPhoto);
 
-    // Fetch the photo data by ID
-    const photoData: Photo[] = await getPhotoById(flags.id);
-
-    // Render the table with the provided photo data
-    const table = TtyTable(TABLE_HEADERS, photoData);
-    this.log(table.render());
+    if (flags.id) { // Fetch the photo data by ID
+      const photoData: Photo[] = await getPhotoById(flags.id);
+      // Render the table with the provided photo data
+      const table = TtyTable(TABLE_HEADERS, photoData);
+      this.log(table.render());
+    } else if (flags.name) { // Fetch the photo(s) data by name or keyword in name
+      const albums: Album[] = await getAllAlbums();
+      const photoData: Photo[] = albums.reduce((photoGroups: Photo[], album: Album) => {
+        const photos = album.photos.filter((photo: Photo) => photo.title.toLowerCase().includes(flags.name!.toLowerCase()));
+        return photoGroups.concat(photos);
+      }, []);
+      const table = TtyTable(TABLE_HEADERS, photoData);
+      this.log(table.render());
+      return;
+    } else {
+      this.error('Please provide a photo ID or name');
+    }
   }
 }
